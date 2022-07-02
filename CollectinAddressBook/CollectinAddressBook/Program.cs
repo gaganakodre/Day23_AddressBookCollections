2a06684bfec85c93821007e8526b765060148a8b
shreegowri <gaganandh@gmail.com>
2022-07-02 06:11:30 +0530

    [shreegowri]Addressbook welcome message

---
+++ CollectinAddressBook/CollectinAddressBook/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CoolectionAddressBook
+{
+   class Program
+    {
+        public static void Main(string[]args)
+        {
+            Console.WriteLine("welcome to addressBook");
+        }
+    }
+}
